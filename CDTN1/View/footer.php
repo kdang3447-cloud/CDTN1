f75af1e846c379f85bb64b72faf2164cc89728c2
trankhanhkim2004-blip <trankhanhkim2004@gmail.com>
2026-03-30 11:34:23 +0700

    trangchu

---
+++ CDTN1/View/footer.php
@@ -0,0 +1,9 @@
+</main>
+
+    <footer class="site-footer">
+        <div class="container">
+            <p>&copy; <?= date('Y') ?> Cửa hàng bán sách - PHP MVC</p>
+        </div>
+    </footer>
+</body>
+</html>
