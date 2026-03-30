4d1f422dfafd4d603647a6c14de64e97b23da9a1
trankhanhkim2004-blip <trankhanhkim2004@gmail.com>
2026-03-30 11:37:27 +0700

    index.php

--- CDTN1/index.php
+++ CDTN1/index.php
@@ -1,5 +1,31 @@
 <?php
-require_once __DIR__ . '/app/controllers/HomeController.php';
+define('BASE_PATH', __DIR__);
 
-$controller = new HomeController();
-$controller->index();
+spl_autoload_register(function ($class) {
+    $paths = [
+        BASE_PATH . '/app/controllers/' . $class . '.php',
+        BASE_PATH . '/app/models/' . $class . '.php'
+    ];
+
+    foreach ($paths as $file) {
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+});
+
+$controllerName = ucfirst(strtolower($_GET['controller'] ?? 'home')) . 'Controller';
+$action = $_GET['action'] ?? 'index';
+
+if (!class_exists($controllerName)) {
+    die("Controller không tồn tại.");
+}
+
+$controller = new $controllerName();
+
+if (!method_exists($controller, $action)) {
+    die("Action không tồn tại.");
+}
+
+$controller->$action();
